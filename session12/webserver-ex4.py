eb2f41cdd7025a1fc04d866be8c1c6daab51b100
jbriz <j.briz.2020@alumnos.urjc.es>
2021-04-21 13:31:06 +0200

    p4

--- session12/webserver-ex4.py
+++ session12/webserver-ex4.py
@@ -33,7 +33,19 @@ def process_client(s):
     # Body (content to send)
 
     # This new contents are written in HTML language
-    body = HTML/index.html
+    body = """
+    <!DOCTYPE html>
+    <html lang="en" dir="ltr">
+      <head>
+        <meta charset="utf-8">
+        <title>Green server</title>
+      </head>
+      <body style="background-color: lightgreen;">
+        <h1>GREEN SERVER</h1>
+        <p>I am the Green Server! :-)</p>
+      </body>
+    </html>
+    """
     # -- Status line: We respond that everything is ok (200 code)
     status_line = "HTTP/1.1 200 OK\n"
 
